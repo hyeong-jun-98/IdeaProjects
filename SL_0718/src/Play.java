bfc2a1df9a2c0db4073ac817b2aba62957be1511
Yang <bellehee98@naver.com>
2022-07-18 18:04:42 +0900

    7/18 로또

---
+++ SL_0718/src/Play.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Play {
+    public static void main(String[] args) {
+        Random rd = new Random();
+        int [] arr = new int[6];
+       for(int i = 0; i < 6; i++) {
+            arr[i] = rd.nextInt(10) + 1;
+            for (int j = 0; j < i; j++) {
+                if(arr[i] == arr[j]) {
+                  i--;
+                }
+            }
+
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < 6; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+
+
+
+    }
+}
--- SL_0718/src/calculator.java
+++ SL_0718/src/calculator.java
@@ -1,35 +1,53 @@
 import java.util.Scanner;
 
 public class calculator {
-    int a,b, add, sub, mul, div;
 
-    public void cal() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("a : ");
-        a = sc.nextInt();
-
-        System.out.print("b : ");
-        b = sc.nextInt();
-
-        add = a + b;
-        sub = a - b;
-        mul = a * b;
-        div = a / b;
-
-        System.out.println("a + b = " + add);
-        System.out.println("a - b = " + sub);
-        System.out.println("a * b = " + mul);
-        System.out.println("a / b = " + div);
-
-    }
 
 
-    public static void main(String [] args) {
 
-        calculator cl = new calculator();
-        cl.cal();
 
+
+    /*    String name = null;
+        int weight = 0;
+        double height = 0.0;
+
+        System.out.print("name : ");
+        name = sc.nextLine();
+        System.out.print("height : ");
+        height = sc.nextDouble();
+        System.out.print("weight : ");
+        weight = sc.nextInt();
+
+        double bmi = weight / (height * height);
+
+        if(bmi > 30) {
+            System.out.println("비만");;
+        } else if(bmi >= 25 && bmi <= 29) {
+            System.out.println("과체중");
+        } else if(bmi >= 20 && bmi <= 24){
+            System.out.println("정상");
+        } else {
+            System.out.println("저체중");
+        }
+
+        System.out.println(bmi);
+
+*/
+
+//        int a = 60, b = 50, c = 70;
+//        int sum = a + b + c;
+//        double avg = sum / 3.0;  // 변수를 쓰면 램의 공간을 차지하기 때문에..
+//
+//        if (a < 40 || b < 40 || c < 40) {
+//            System.out.println("불합격");
+//        } else if(avg >= 60) {
+//            System.out.println("합격");
+//        } else {
+//            System.out.println("불합격");
+//        }
 
     }
 }
